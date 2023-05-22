f0cb89ed6530088b7116fa42a89099b7979dde3e
puertocampo <puertocampo@gmail.com>
2023-05-22 13:25:13 +0900

    [add] LinkButton

---
+++ src/components/atoms/LinkButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <a :href="link" target="_blank"><slot /></a>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    link: String,
+  },
+})
+export default class LinkButton extends Vue {
+  link!: string;
+}
+</script>
+
+<style lang="scss" scoped>
+a {
+  background-color: "#000";
+  color: "#787";
+}
+</style>
